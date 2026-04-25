85055148374a3263f8e2af2fffd4bc07ac69ca3a
Marley Puyat <marley@puyat.com.au>
2026-04-25 12:23:34 +1000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-layout">
+      <aside className="source-panel">Source Panel</aside>
+      <main className="editor-panel">Editor Panel</main>
+    </div>
+  )
+}
+
+export default App
